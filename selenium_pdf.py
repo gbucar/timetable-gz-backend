5f0193e9424b2d659f41253b6b8a2ee81f1420ea
gbucar <gregor.bucar99@gmail.com>
2021-09-12 00:14:31 +0200

    heroku

--- selenium_pdf.py
+++ selenium_pdf.py
@@ -6,20 +6,31 @@ import time
 import pyperclip
 import json
 
+
+
 class TimetableFetch:
-    def __init__(self):
+    def  load_driver(self):
         options = webdriver.FirefoxOptions()
-        options.add_argument("-headless")        
-        options.add_argument("-disable-gpu")        
-        options.add_argument("-no-sandbox") 
         
+        # enable trace level for debugging 
+        options.log.level = "trace"
+
+        options.add_argument("-remote-debugging-port=9224")
+        options.add_argument("-headless")
+        options.add_argument("-disable-gpu")
+        options.add_argument("-no-sandbox")
+
         binary = FirefoxBinary(os.environ.get('FIREFOX_BIN'))
 
-        self.driver = webdriver.Firefox(
+        firefox_driver = webdriver.Firefox(
             firefox_binary=binary,
             executable_path=os.environ.get('GECKODRIVER_PATH'),
-            options=options
-            )
+            options=options)
+
+        return firefox_driver
+
+    def __init__(self):
+        self.driver = self.load_driver()
         self.unknown = []
         self.ignored = ['športni', 'dan', '-', 'Posočje', 'športni', 'dan', '-', 'Posočje', 'ekskurzija', 'na', 'Koroško', 'športni', 'dan', '–', 'Bohinj', 'sistematski', 'pregled', 'študijsko', 'potovanje', 'v', 'Španijo', 'študijsko', 'potovanje', 'v', 'Španijo', 'URE', '7.20', '-', '8.05', '8.10', '-', '8.55', '9.00', '-', '9.45', '10.05', '-', '10.50', '10.55', '-', '11.40', '11.45', '-', '12.30', '12.35', '-', '13.20', '13.50', '-', '14.35', '13.00', '-', '13.45', 'f']
         self.matura = ['mO1/nOV/', 'n4/š1/š3a', 'geo1/nOV/š2', 'mV2/mO3\r\n/n3/š3b', 'mO1/nOV/\nn4/š1/š3a', 'geo1/nOV\n/š2', 'mV2/mO3\n/n3/š3b', 'n1/psi1', 'n26/š3c6/geo26/bio1/']
