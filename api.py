00a6d88549e9bf01d011b2395fb41c4edc70c32b
gbucar <gregor.bucar99@gmail.com>
2021-09-11 22:58:22 +0200

    heroku

--- api.py
+++ api.py
@@ -1,11 +1,7 @@
 from flask import Flask
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
 
-class Timetable(Resource):
-    def get(self):
-        return "self.t.get_timetable(), 200"
-
-api.add_resource(Timetable, "/")
+@app.route("/")
+def home():
+    return "Hello, World!"
